352d03c58416344c6ba1700ce8a786339a5411aa
RaviiSharma <engineerravi036@gmail.com>
2022-09-17 12:00:08 +0530

    worked on seven apis and handle all edge cases

--- src/Models/AuthorModel.js
+++ src/Models/AuthorModel.js
@@ -1,32 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const authorSchema = new mongoose.Schema({
-    fname: {
-        type: String,
-        required: true,
-        trim:true
-    },
-    lname: {
-        type: String,
-        required: true,
-        trim:true
-    },
-    title: {
-        type: String,
-        required: true,
-        enum: ["Mr", "Mrs", "Miss"]
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required:true,
-        unique: true
-    }
+const authorSchema = new mongoose.Schema(
+  {
+    fname: { type: String, required: true, trim: true },
 
-}, { timestamps: true });
+    lname: { type: String, required: true, trim: true },
 
-module.exports = mongoose.model('Author', authorSchema)
+    title: { type: String, required: true, enum: ["Mr", "Mrs", "Miss"] },
+
+    email: { type: String, required: true, unique: true },
+
+    password: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Author", authorSchema);
